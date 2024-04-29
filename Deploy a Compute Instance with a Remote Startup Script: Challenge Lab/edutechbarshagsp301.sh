d17f0c43fbed6bf8c6f2ac4d286389afe9845901
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-04-29 19:54:22 +0530

    Update edutechbarshagsp301.sh

--- Deploy a Compute Instance with a Remote Startup Script: Challenge Lab/edutechbarshagsp301.sh
+++ Deploy a Compute Instance with a Remote Startup Script: Challenge Lab/edutechbarshagsp301.sh
@@ -3,7 +3,7 @@ gsutil mb gs://$DEVSHELL_PROJECT_ID
 
 gsutil cp gs://sureskills-ql/challenge-labs/ch01-startup-script/install-web.sh gs://$DEVSHELL_PROJECT_ID
 
-gcloud compute instances create techcps --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=n1-standard-1 --tags=http-server --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID/install-web.sh
+gcloud compute instances create edutechbarsha --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=n1-standard-1 --tags=http-server --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID/install-web.sh
 
 
 gcloud compute firewall-rules create allow-http \
