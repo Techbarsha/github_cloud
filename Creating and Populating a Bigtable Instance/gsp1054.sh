12318167dc67530ebf8239535187a90790d430e0
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-05-23 23:05:22 +0530

    Update gsp1054.sh

--- Creating and Populating a Bigtable Instance/gsp1054.sh
+++ Creating and Populating a Bigtable Instance/gsp1054.sh
@@ -1,11 +1,51 @@
+
+
+export REGION="${ZONE%-*}"
+
+# Disable Dataflow API
 gcloud services disable dataflow.googleapis.com
+
+# Enable Dataflow API
 gcloud services enable dataflow.googleapis.com
-export REGION=${ZONE%-*}
-gcloud bigtable instances create personalized-sales \
-            --display-name="Personalized Sales" \
-            --cluster-config=id=personalized-sales-c1,zone=$ZONE
-cbt -instance personalized-sales createtable UserSessions
-cbt -instance personalized-sales createfamily UserSessions Interactions
-cbt -instance personalized-sales createfamily UserSessions Sales
-cbt -instance personalized-sales lookup UserSessions
-gsutil mb gs://$DEVSHELL_PROJECT_ID/
+
+
+gcloud bigtable instances tables create UserSessions --project=$DEVSHELL_PROJECT_ID --instance=personalized-sales --column-families=Interactions,Sales
+
+gsutil mb -l US gs://$DEVSHELL_PROJECT_ID
+
+sleep 60
+
+
+#!/bin/bash
+
+deploy_function() {
+  gcloud dataflow jobs run import-usersessions \
+    --region=$REGION \
+    --gcs-location gs://dataflow-templates-$REGION/latest/GCS_SequenceFile_to_Cloud_Bigtable \
+    --staging-location gs://$DEVSHELL_PROJECT_ID/temp \
+    --parameters bigtableProject=$DEVSHELL_PROJECT_ID,bigtableInstanceId=personalized-sales,bigtableTableId=UserSessions,sourcePattern=gs://cloud-training/OCBL377/retail-interactions-sales-00000-of-00001,mutationThrottleLatencyMs=0
+}
+
+deploy_success=false
+
+while [ "$deploy_success" = false ]; do
+  if deploy_function; then
+    echo "Function deployed successfully. (https://www.youtube.com/@edutechbarsha)"
+    deploy_success=true
+  else
+    echo "Deployment Retrying, please subscribe to Edutech Barsha (https://www.youtube.com/@edutechbarsha).."
+    sleep 10
+  fi
+done
+
+
+
+
+echo project = `gcloud config get-value project` \
+    >> ~/.cbtrc
+
+echo instance = personalized-sales \
+    >> ~/.cbtrc
+
+cat ~/.cbtrc
+
