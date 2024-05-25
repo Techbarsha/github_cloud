a2f97503f8d3334ebc31a6e51a83a7eb35423869
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-05-26 02:21:11 +0530

    Update gsp421.sh

--- APIs Explorer: Cloud Storage/gsp421.sh
+++ APIs Explorer: Cloud Storage/gsp421.sh
@@ -5,11 +5,11 @@ gsutil mb gs://$DEVSHELL_PROJECT_ID
 gsutil mb gs://$DEVSHELL_PROJECT_ID-2
 
 
-curl -LO raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/APIs%20Explorer%20Cloud%20Storage/demo-image1.png
+curl -LO raw.githubusercontent.com/Techbarsha/github_cloud/main/APIs%20Explorer%3A%20Cloud%20Storage/demo-image1.png
 
-curl -LO raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/APIs%20Explorer%20Cloud%20Storage/demo-image2.png
+curl -LO raw.githubusercontent.com/Techbarsha/github_cloud/main/APIs%20Explorer%3A%20Cloud%20Storage/demo-image2.png
 
-curl -LO raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/APIs%20Explorer%20Cloud%20Storage/demo-image1-copy.png
+curl -LO raw.githubusercontent.com/Techbarsha/github_cloud/main/APIs%20Explorer%3A%20Cloud%20Storage/demo-image1-copy.png
 
 
 gsutil cp demo-image1.png gs://$DEVSHELL_PROJECT_ID/demo-image1.png
