bbfcd2d2a418c573c0d8760c0870f1ef627c9b3c
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-07-10 21:52:25 +0530

    Update gsp277.sh

--- APIs Explorer: Qwik Start/gsp277.sh
+++ APIs Explorer: Qwik Start/gsp277.sh
@@ -1,15 +1,40 @@
+#!/bin/bash
+# Define color variables
 
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 
-gcloud config set project $DEVSHELL_PROJECT_ID
-gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket
-gsutil bucketpolicyonly set off gs://$DEVSHELL_PROJECT_ID-bucket
-gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-bucket
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
 
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
 
-wget https://github.com/quiccklabs/Labs_solutions/blob/master/Use%20APIs%20to%20Work%20with%20Cloud%20Storage%20Challenge%20Lab/world.jpeg
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
-mv world.jpeg demo-image.jpg
+export BUCKET="$(gcloud config get-value project)"         
 
-gsutil cp demo-image.jpg  gs://$DEVSHELL_PROJECT_ID-bucket
+gsutil mb -p $BUCKET gs://$BUCKET-bucket
 
-gsutil acl ch -u allUsers:R gs://$DEVSHELL_PROJECT_ID-bucket/demo-image.jpg
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/APIs%20Explorer%20Qwik%20Start/demo-image.jpg
+
+gsutil cp demo-image.jpg gs://$BUCKET-bucket/demo-image.jpg
+
+gsutil acl ch -u allUsers:R gs://$BUCKET-bucket/demo-image.jpg
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab!! SUBSCRIBE NOW !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
