14d3160662739f37dd6e7ca39f8d0e6ea34ebaa4
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-10-15 03:05:04 +0530

    Update edutechbarsha.sh

--- Caching Content with Cloud CDN/edutechbarsha.sh
+++ Caching Content with Cloud CDN/edutechbarsha.sh
@@ -1,23 +1,41 @@
-# Caching Content with Cloud CDN
+#!/bin/bash
+# Define color variables
 
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 
-gcloud auth list
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_RED}${BOLD}Starting Execution!!${RESET}"
 
 export PROJECT_ID=$(gcloud config get-value project)
 
-export BUCKET_NAME="$PROJECT_ID"
+gsutil mb -p $DEVSHELL_PROJECT_ID -c STANDARD -l $REGION -b on gs://$DEVSHELL_PROJECT_ID
 
-gsutil mb -l US gs://$BUCKET_NAME
+gsutil uniformbucketlevelaccess set off gs://$DEVSHELL_PROJECT_ID
 
-gsutil cp gs://cloud-training/gcpnet/cdn/cdn.png gs://$BUCKET_NAME
+gsutil cp gs://cloud-training/gcpnet/cdn/cdn.png gs://$DEVSHELL_PROJECT_ID
 
-gsutil acl ch -u AllUsers:R gs://$BUCKET_NAME/cdn.png
+gsutil acl ch -u AllUsers:R gs://$DEVSHELL_PROJECT_ID/cdn.png
 
-gsutil iam ch allUsers:objectViewer gs://$BUCKET_NAME
-
-# Get the access token
 TOKEN_ID=$(gcloud auth application-default print-access-token)
-
 
 # Create a Backend Bucket
 curl -X POST -H "Content-Type: application/json" \
@@ -50,7 +68,6 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$PROJECT_ID/global/urlMaps"
 
-
 sleep 15
 
 # Create a target HTTP proxy for the CDN load balancer
@@ -77,3 +94,7 @@ curl -X POST -H "Content-Type: application/json" \
     "target": "projects/'"$PROJECT_ID"'/global/targetHttpProxies/cdn-lb-target-proxy"
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$PROJECT_ID/global/forwardingRules"
+
+  echo "${BG_BLUE}${BOLD}Congratulations For Completing The Lab! Subscribe Edutech Barsha!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
