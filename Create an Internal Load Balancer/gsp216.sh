d1c17522496837a93b41f185dda73e3057e7390a
Barsha <122685155+Techbarsha@users.noreply.github.com>
2025-02-02 01:33:05 +0530

    Update gsp216.sh

--- Create an Internal Load Balancer/gsp216.sh
+++ Create an Internal Load Balancer/gsp216.sh
@@ -1,83 +1,123 @@
+#!/bin/bash
+# Define color variables
 
-export REGION=${ZONE%-*}
-# export REGION2=${ZONE2%-*}
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 
-gcloud compute firewall-rules create app-allow-http --network=my-internal-app --direction=INGRESS --action=ALLOW --target-tags=lb-backend --source-ranges=0.0.0.0/0 --rules=tcp:80
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
 
-gcloud compute firewall-rules create app-allow-health-check --network default --action allow --direction INGRESS --target-tags lb-backend --source-ranges 130.211.0.0/22,35.191.0.0/16 --rules tcp
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
 
-gcloud compute instance-templates create instance-template-1 --machine-type=e2-medium --network=my-internal-app --region $REGION --subnet=subnet-a --tags=lb-backend --metadata=startup-script-url=gs://cloud-training/gcpnet/ilb/startup.sh
+echo "${BG_MAGENTA}${BOLD}Now Starting Execution!!${RESET}"
 
-gcloud compute instance-templates create instance-template-2 --machine-type=e2-medium --network=my-internal-app --region $REGION --subnet=subnet-b --tags=lb-backend --metadata=startup-script-url=gs://cloud-training/gcpnet/ilb/startup.sh
+export REGION="${ZONE%-*}"
+gcloud config set compute/region $REGION
+gcloud config set compute/zone $ZONE
 
-gcloud compute instance-groups managed create instance-group-1 --base-instance-name=instance-group-1 --template=instance-template-1 --zone=$ZONE --size=1
+sudo apt-get install -y virtualenv
 
-gcloud compute instance-groups managed set-autoscaling instance-group-1 --zone=$ZONE --cool-down-period=45 --max-num-replicas=5 --min-num-replicas=1 --target-cpu-utilization=0.8
+python3 -m venv venv
 
-gcloud compute instance-groups managed create instance-group-2 --base-instance-name=instance-group-2 --template=instance-template-2 --zone=$ZONE2 --size=1
+source venv/bin/activate
 
-gcloud compute instance-groups managed set-autoscaling instance-group-2 --zone=$ZONE2 --cool-down-period=45 --max-num-replicas=5 --min-num-replicas=1 --target-cpu-utilization=0.8
+echo 'sudo chmod -R 777 /usr/local/sbin/
+sudo cat << EOF > /usr/local/sbin/serveprimes.py
+import http.server
+def is_prime(a): return a!=1 and all(a % i for i in range(2,int(a**0.5)+1))
+class myHandler(http.server.BaseHTTPRequestHandler):
+  def do_GET(s):
+    s.send_response(200)
+    s.send_header("Content-type", "text/plain")
+    s.end_headers()
+    s.wfile.write(bytes(str(is_prime(int(s.path[1:]))).encode("utf-8")))
+http.server.HTTPServer(("",80),myHandler).serve_forever()
+EOF
+nohup python3 /usr/local/sbin/serveprimes.py >/dev/null 2>&1 &' > backend.sh
 
-gcloud compute instances create utility-vm --zone=$ZONE --machine-type=e2-micro --image-family=debian-10 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --network=my-internal-app --subnet=subnet-a --private-network-ip=10.10.20.50
+gcloud compute instance-templates create primecalc \
+--metadata-from-file startup-script=backend.sh \
+--no-address --tags backend --machine-type=e2-medium
 
-gcloud compute health-checks create tcp my-ilb-health-check --description="subscribe to techcps" --check-interval=5s --timeout=5s --unhealthy-threshold=2 --healthy-threshold=2 --port=80 --proxy-header=NONE
+gcloud compute firewall-rules create http --network default --allow=tcp:80 \
+--source-ranges 10.142.0.0/20 --target-tags backend
 
-TOKEN=$(gcloud auth application-default print-access-token)
+gcloud compute instance-groups managed create backend \
+--size 3 \
+--template primecalc \
+--zone $ZONE
 
-cat > cp1.json <<EOF_END
-{
-    "backends": [
-      {
-        "balancingMode": "CONNECTION",
-        "group": "projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/instanceGroups/instance-group-1"
-      },
-      {
-        "balancingMode": "CONNECTION",
-        "group": "projects/$DEVSHELL_PROJECT_ID/zones/$ZONE2/instanceGroups/instance-group-2"
-      }
-    ],
-    "connectionDraining": {
-      "drainingTimeoutSec": 300
-    },
-    "description": "",
-    "healthChecks": [
-      "projects/$DEVSHELL_PROJECT_ID/global/healthChecks/my-ilb-health-check"
-    ],
-    "loadBalancingScheme": "INTERNAL",
-    "logConfig": {
-      "enable": false
-    },
-    "name": "my-ilb",
-    "network": "projects/$DEVSHELL_PROJECT_ID/global/networks/my-internal-app",
-    "protocol": "TCP",
-    "region": "projects/$DEVSHELL_PROJECT_ID/regions/$REGION",
-    "sessionAffinity": "NONE"
-  }
-EOF_END
+gcloud compute instance-groups managed set-autoscaling backend \
+--target-cpu-utilization 0.8 --min-num-replicas 3 \
+--max-num-replicas 10 --zone $ZONE
 
-cat > cp2.json <<EOF_END
-{
-   "IPAddress": "10.10.30.5",
-   "loadBalancingScheme": "INTERNAL",
-   "allowGlobalAccess": false,
-   "description": "subscribe to techcps",
-   "ipVersion": "IPV4",
-   "backendService": "projects/$DEVSHELL_PROJECT_ID/regions/$REGION/backendServices/my-ilb",
-   "IPProtocol": "TCP",
-   "networkTier": "PREMIUM",
-   "name": "my-ilb-forwarding-rule",
-   "ports": [
-     "80"
-   ],
-   "region": "projects/$DEVSHELL_PROJECT_ID/regions/$REGION",
-   "subnetwork": "projects/$DEVSHELL_PROJECT_ID/regions/$REGION/subnetworks/subnet-b"
- }
-EOF_END
+gcloud compute health-checks create http ilb-health --request-path /2
 
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d  @cp1.json \
-"https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/regions/$REGION/backendServices"
+gcloud compute backend-services create prime-service \
+--load-balancing-scheme internal --region=$REGION \
+--protocol tcp --health-checks ilb-health
 
-sleep 17
+gcloud compute backend-services add-backend prime-service \
+--instance-group backend --instance-group-zone=$ZONE \
+--region=$REGION
 
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d @cp2.json \
-"https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/regions/$REGION/forwardingRules"
+gcloud compute forwarding-rules create prime-lb \
+--load-balancing-scheme internal \
+--ports 80 --network default \
+--region=$REGION --address $STATIC_IP \
+--backend-service prime-service
+
+echo 'sudo chmod -R 777 /usr/local/sbin/
+sudo cat << EOF > /usr/local/sbin/getprimes.py
+import urllib.request
+from multiprocessing.dummy import Pool as ThreadPool
+import http.server
+PREFIX="http://10.128.10.10/" #HTTP Load Balancer
+def get_url(number):
+    return urllib.request.urlopen(PREFIX+str(number)).read()
+class myHandler(http.server.BaseHTTPRequestHandler):
+  def do_GET(s):
+    s.send_response(200)
+    s.send_header("Content-type", "text/html")
+    s.end_headers()
+    i = int(s.path[1:]) if (len(s.path)>1) else 1
+    s.wfile.write("<html><body><table>".encode('utf-8'))
+    pool = ThreadPool(10)
+    results = pool.map(get_url,range(i,i+100))
+    for x in range(0,100):
+      if not (x % 10): s.wfile.write("<tr>".encode('utf-8'))
+      if results[x]=="True":
+        s.wfile.write("<td bgcolor='#00ff00'>".encode('utf-8'))
+      else:
+        s.wfile.write("<td bgcolor='#ff0000'>".encode('utf-8'))
+      s.wfile.write(str(x+i).encode('utf-8')+"</td> ".encode('utf-8'))
+      if not ((x+1) % 10): s.wfile.write("</tr>".encode('utf-8'))
+    s.wfile.write("</table></body></html>".encode('utf-8'))
+http.server.HTTPServer(("",80),myHandler).serve_forever()
+EOF
+nohup python3 /usr/local/sbin/getprimes.py >/dev/null 2>&1 &' > frontend.sh
+
+gcloud compute instances create frontend --zone=$ZONE \
+--metadata-from-file startup-script=frontend.sh \
+--tags frontend --machine-type=e2-standard-2
+
+gcloud compute firewall-rules create http2 --network default --allow=tcp:80 \
+--source-ranges 0.0.0.0/0 --target-tags frontend
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab! Subscribe Now!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
