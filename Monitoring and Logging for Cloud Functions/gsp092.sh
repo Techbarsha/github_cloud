131c5f9b0ea6b39f19fa92fe2aea58dc836982f8
Barsha <122685155+Techbarsha@users.noreply.github.com>
2025-04-10 16:07:51 +0530

    Update gsp092.sh

--- Monitoring and Logging for Cloud Functions/gsp092.sh
+++ Monitoring and Logging for Cloud Functions/gsp092.sh
@@ -1,29 +1,4 @@
-#!/bin/bash
-# Define color variables
 
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
-
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
-
-BOLD=`tput bold`
-RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
-
-echo "${BG_RED}${BOLD}Starting Execution Now!!${RESET}"
 
 gcloud services enable \
   artifactregistry.googleapis.com \
@@ -34,8 +9,11 @@ gcloud services enable \
   logging.googleapis.com \
   pubsub.googleapis.com
 
+sleep 60
+
 mkdir ~/hello-http && cd $_
 touch index.js && touch package.json
+
 
 cat > index.js <<EOF
 /**
@@ -51,6 +29,7 @@ exports.helloWorld = (req, res) => {
   
 EOF
 
+
 cat > package.json <<EOF
 {
     "name": "sample-http",
@@ -60,39 +39,45 @@ cat > package.json <<EOF
 EOF
 
 
-
-deploy_function() {  gcloud functions deploy helloWorld \
---trigger-http \
---runtime nodejs20 \
---allow-unauthenticated \
---region $REGION \
---no-gen2 \
---max-instances 5
+# Your existing deployment command
+deploy_function() {
+    gcloud functions deploy helloWorld \
+    --trigger-http \
+    --runtime nodejs20 \
+    --allow-unauthenticated \
+    --region $REGION \
+    --max-instances 5 \
+    --no-gen2 \
+    --quiet
 }
 
-deploy_success=false
+# Variables
+SERVICE_NAME="helloWorld"
 
-while [ "$deploy_success" = false ]; do
-  if deploy_function; then
-    echo "Cloud Run service is created. Exiting the loop."
-    deploy_success=true
+# Loop until the Cloud Function is deployed
+while true; do
+  # Run the deployment command
+  deploy_function
+
+  # Check if Cloud Function is deployed
+  if gcloud functions describe $SERVICE_NAME --region $REGION &> /dev/null; then
+    echo "Cloud Function is deployed. Exiting the loop."
+    break
   else
-    echo "Waiting for Cloud Run service to be created..."
-    sleep 60
+    echo "Waiting for Cloud Function to be deployed..."
+    echo "Meantime Subscribe to Quicklab[https://www.youtube.com/@quick_lab]."
+    sleep 10
   fi
 done
 
-echo "Running the next code..."
 
 curl -LO 'https://github.com/tsenart/vegeta/releases/download/v6.3.0/vegeta-v6.3.0-linux-386.tar.gz'
 
 tar xvzf vegeta-v6.3.0-linux-386.tar.gz
 
+
+
 gcloud logging metrics create CloudFunctionLatency-Logs \
     --project=$DEVSHELL_PROJECT_ID \
-    --description="awesome lab" \
+    --description="subscribe to quicklab" \
     --log-filter='resource.type="cloud_function" AND resource.labels.function_name="helloWorld" AND log_name="projects/$DEVSHELL_PROJECT_ID/logs/cloudaudit.googleapis.com%2Factivity" AND resource.labels.region="$REGION"'
-
-echo "${BG_BLUE}${BOLD}Great Job!! Congratulations For Completing The Lab!! Subscribre Now!!${RESET}"
-
-#-----------------------------------------------------end----------------------------------------------------------#
